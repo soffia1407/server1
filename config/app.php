e5b2915cc576ebd844c83e0dd64519ea4e460372
soffia1407 <sff140705@gmail.com>
2025-05-13 23:18:02 +0700

    Implementation of access rights | added singleton parser and developed midleware

--- config/app.php
+++ config/app.php
@@ -3,5 +3,10 @@ return [
    //Класс аутентификации
    'auth' => \Src\Auth\Auth::class,
    //Клас пользователя
-   'identity'=>\app\Model\User::class
+   'identity'=>\app\Model\User::class,
+   //Классы для middleware
+   'routeMiddleware' => [
+   'auth' => \Middlewares\AuthMiddleware::class,
+]
+
 ];
